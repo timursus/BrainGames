d505238d05f914697e68a09cde3fbd91dba3d8d4
Timursus <redundant09@gmail.com>
2020-02-27 12:18:03 +0300

    fix export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const userNameQuestion = () => {
+const userNameQuestion = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
+
+export default userNameQuestion;
